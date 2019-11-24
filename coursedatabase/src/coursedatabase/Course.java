c0a06b8c4f5ec8c2ec1e68e0a0af699e132dfd30
ghigbie <gehigbie@gmail.com>
2019-11-23 18:28:37 -0700

    Remove src folder and add new basic packages

--- coursedatabase/src/coursedatabase/Course.java
+++ coursedatabase/src/coursedatabase/Course.java
@@ -1,4 +1,22 @@
 package coursedatabase;
 
 public class Course {
+    public String courseName;
+    public String courseAuthor;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseAuthor() {
+        return courseAuthor;
+    }
+
+    public void setCourseAuthor(String courseAuthor) {
+        this.courseAuthor = courseAuthor;
+    }
 }
